7d50fa88cc70107890aa92e073080de8224f0e03
James Cho <hwisoocho@gmail.com>
2018-03-23 16:34:46 -0700

    week 2 assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService } from './services/promotion.service';
-
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -39,7 +39,7 @@ import { PromotionService } from './services/promotion.service';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
