11299a8be36df5d6b1aa57639de520c4e99e6a79
James Cho <hwisoocho@gmail.com>
2018-03-28 17:01:15 -0700

    Promise part 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -16,8 +16,10 @@ export class MenuComponent implements OnInit {
     selectedDish: Dish;
 
   constructor(private dishService: DishService) { }
+  
   ngOnInit() {
-    this.dishes = this.dishService.getDishes();
+     this.dishService.getDishes()
+     .then(dishes => this.dishes = dishes );
   }
 
   onSelect(dish: Dish) {
